836a12b0db973329108f203bde9aa05094f6d7c6
Treek <nbatre1@yahoo.com>
2020-02-23 08:27:00 -0600

    keep moving forward

---
+++ temps.py
@@ -0,0 +1,27 @@
+
+temps = [221,234,340,230]
+
+
+new_temps = []
+
+
+for temp in temps:
+     new_temps.append(temp/10)
+
+
+
+new_cal =[20,29,47,100]
+
+
+cal = []
+
+
+for cals in new_cal:
+    cal.append(cals/2)
+
+
+
+
+print(new_temps)
+
+print(cal)
