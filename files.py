dfc2f2612cd9b83a3af17bed0e0b56976df2bae2
Treek <nbatre1@yahoo.com>
2020-02-24 12:00:00 -0600

    keep moving forward

--- files.py
+++ files.py
@@ -1,5 +1,16 @@
-myfile = open("fruits.txt")
+# myfile = open("fruits.txt")
+
+# content = myfile.read()
+# myfile.close()
+
+# print(content)
+
+# print(content)
 
 
+# another way to read files/ the with also applies the open and close file for you.
 
-print(myfile.read())
+with open("fruits.txt") as myfile:
+    content = myfile.read()
+    
+print(content)
